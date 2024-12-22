a6c6d7b7601c707e20cda197b7d20d7e705b79df
minoru-laptop <aberusmail0810@gmail.com>
2024-12-22 17:52:21 +0900

    added commandliner

--- back/src/main/java/com/example/my_todo2_back/MyTodo2BackApplication.java
+++ back/src/main/java/com/example/my_todo2_back/MyTodo2BackApplication.java
@@ -1,15 +1,27 @@
 package com.example.my_todo2_back;
 
+import com.example.my_todo2_back.model.Task;
+import com.example.my_todo2_back.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyTodo2BackApplication {
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(MyTodo2BackApplication.class, args);
-		System.out.println("Hello World");
+
+		Task task = new Task(1L, "Task 1", "John", "incomplete");
+		System.out.println(task);
+
+		TaskService taskService = new TaskService();
+		taskService.createTask(task);
+
+
+
 
 	}
 
--- back/src/main/java/com/example/my_todo2_back/model/Task.java
+++ back/src/main/java/com/example/my_todo2_back/model/Task.java
@@ -9,14 +9,59 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String assignee;
     private String status;
+
+    public Task(Long id, String name, String assignee, String status) {
+        this.id = id;
+        this.name = name;
+        this.assignee = assignee;
+        this.status = status;
+    }
+
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 
     public void setStatus(String status) {
         this.status = status;
     }
 
-    // Getters and setters
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", assignee='" + assignee + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
